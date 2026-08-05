ec8a88cf1e77dc9b3c144e201f1e1c669eb76a94
SUBHA SHIL <subha.whitehat@gmail.com>
2026-08-05 09:33:57 +0530

    Methods started

---
+++ src/Methods/printTable.java
@@ -0,0 +1,13 @@
+package Methods;
+
+public class printTable {
+    static void print2Table() {
+        for (int i = 1; i <= 10; i ++) {
+            int ans = 2 * i;
+            System.out.println("-->" + ans);
+        }
+    }
+     public static void main(String[] args) {
+        print2Table();
+    }
+}
